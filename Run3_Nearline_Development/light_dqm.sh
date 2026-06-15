19c10215dd3362c592ece346a39fb7e98de8f5bf
Angela White <123964490+AWh1t3@users.noreply.github.com>
2026-06-15 15:17:51 -0500

    Bash script for running LRS DQM

---
+++ Run3_Nearline_Development/light_dqm.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+stage=light_dqm
+
+source $(dirname $BASH_SOURCE)/../lib/init.inc.sh
+
+inbase=$nearline_root/flowed_light
+
+# For example,
+inpath=$1; shift
+indir=$(dirname "$inpath")
+reldir=$(echo "$indir" | sed "s|^$inbase/||")
+
+# Remove the extension and the final numbers. The final numbers are either the
+# run number (123 if the filename is like "mpd_run_foo_rctl_123.FLOW.hdf5") or
+# the subrun number (789 if the filename is like
+# "mpd_run_bar_rctl_456_p789.FLOW.hdf5"). We end up with a file_syntax of either
+# "mpd_run_foo_rctl_" or "mpd_run_bar_rctl_456_p"
+file_syntax=$(basename "$inpath" .FLOW.hdf5 | sed 's/[0-9]\+$//')
+
+# Extract the same final numbers we stripped out above (welcome to regex hell)
+start_run=$(basename "$inpath" .FLOW.hdf5 | sed -n 's/.*[^0-9]\([0-9]\+\)$/\1/p')
+
+get_outpath() {
+    outbase=$1
+    tag=$2
+    ext=$3
+    outname=$(basename "$inpath" .FLOW.hdf5)_light_dqm_${tag}.$ext
+    mkdir -p "$outbase/$reldir"
+    realpath "$outbase/$reldir/$outname"
+}
+
+channel_status=$(dirname "${BASH_SOURCE[0]}")/light_dqm/channel_status_warmRun2.csv
+
+plotpath1=$(get_outpath "$plot_outbase" main pdf)
+plotpath2=$(get_outpath "$plot_outbase" baseline png)
+plotpath3=$(get_outpath "$plot_outbase" flatline png)
+
+logpath=$(get_outpath "$log_outbase" log)
+
+cd "$(dirname "${BASH_SOURCE[0]}")/light_dqm"
+
+if [[ "$(stat -c %s "$inpath")" -gt 50000000000 ]]; then
+    echo "File is larger than 50 GB; bailing"
+    exit 1
+fi
+
+# light_dqm.py expects the trailing slash on input_path
+python3 light_dqm.py --input_path "$inbase/$reldir/" \
+                     --output_dir "$plot_outbase" \
+                     --tmp_dir "$plot_outbase/tmp" \
+                     --file_syntax "$file_syntax" \
+                     --output_dir "$(dirname "$plotpath1")" \
+                     --channel_status_file "$channel_status"\
+                     --start_run $start_run \
+                     2>&1 | tee "$logpath"
+
+
+latest_outbase="$plot_outbase/latest"
+latest_path1="$latest_outbase/latest_main.pdf"
+latest_path2="$latest_outbase/latest_baseline.png"
+latest_path3="$latest_outbase/latest_dead_chan.png"
+
+if [ ! -d "$latest_outbase" ]; then
+    mkdir -p "$latest_outbase"
+fi
+
+ln -sf $plotpath1 $latest_path1
+ln -sf $plotpath2 $latest_path2
+ln -sf $plotpath3 $latest_path3
